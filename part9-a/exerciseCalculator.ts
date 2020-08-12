1b1cbca27f3cadea266feeb25f72b45d500b11d2
achianumba <chianumbaarinze@gmail.com>
2020-08-12 07:35:14 +0100

    Refactor code to dynamically call 'calculateExercises'

--- part9-a/exerciseCalculator.ts
+++ part9-a/exerciseCalculator.ts
@@ -3,78 +3,76 @@ interface Summary {
   daysTrained: number;
   target: number;
   averageTime: number;
-  targetReached: boolean,
+  targetReached: boolean;
   rating: number;
   ratingDescription: string;
 }
 
 interface Values {
-  target: number,
-  times: number[]
+  daily_exercises: number[];
+  target: number;
 }
 
-function parseValues(values: string[]): Values {
-  if (!values[3]) {
-    throw Error('Invalid arguments: Verify that "target" and "exerciseTimes" are defined');
+interface Err {
+  error: string;
+  message: string;
+}
+
+function validateInput(values: Values): Values {
+  const targetIsNumber: boolean = typeof values.target === "number";
+  const exercisesTimesAreNumbers: boolean = values.daily_exercises.every(
+    (x) => typeof x === "number"
+  );
+
+  if (!targetIsNumber || !exercisesTimesAreNumbers) {
+    throw Error(
+      "'target' must be a number and 'exercise_times' must be an array of numbers"
+    );
   }
 
-  const target = Number(values[2]);
-  const times = values.slice(3).map(num => Number(num));
-  const timesAreNums = times.every(t => !isNaN(t))
+  return values;
+}
 
-  if (!isNaN(target) && timesAreNums) {
-    return {
+function calculateExercises(args: Values): Summary | Err {
+  try {
+    const { target, daily_exercises } = validateInput(args);
+    let summary = {
+      numDays: daily_exercises.length,
+      daysTrained: daily_exercises.filter((t) => t > 0).length,
       target,
-      times
+      averageTime: 0,
+      targetReached: daily_exercises.every((t) => t >= target),
+      rating: 0,
+      ratingDescription: "",
+    };
+
+    const averageTime =
+      daily_exercises.reduce((acc, t) => acc + t, 0) / daily_exercises.length;
+
+    summary.averageTime = averageTime;
+
+    if (averageTime >= target) {
+      summary.rating = 3;
+      summary.ratingDescription = "Impressive performance";
     }
-  }
 
-  if (isNaN(target)) {
-    throw Error('Invalid argument: "target" must be a number');
-  }
+    if (averageTime < target) {
+      summary.rating = 2;
+      summary.ratingDescription = "Not bad";
+    }
 
-  if (!timesAreNums) {
-    throw Error('Invalid argument: "target" must be a number');
-  }
-}
+    if (averageTime < target / 2) {
+      summary.rating = 1;
+      summary.ratingDescription = "Your performance is low";
+    }
 
-function calculateExercises(timeSpent: number[], targetTime: number): Summary {
-  let summary = {
-    numDays: timeSpent.length,
-    daysTrained: timeSpent.filter(t => t > 0).length,
-    target: targetTime,
-    averageTime: 0,
-    targetReached: timeSpent.every(t => t >= targetTime),
-    rating: 0,
-    ratingDescription: "okay",
-  };
-
-  const averageTime = timeSpent.reduce((acc, t) => acc + t, 0) / timeSpent.length;
-
-  summary.averageTime = averageTime;
-
-  if (averageTime < targetTime / 2) {
-    summary.rating = 1;
-    summary.ratingDescription = "Your performance is low";
-    return summary
-  }
-
-  if (averageTime < targetTime) {
-    summary.rating = 2;
-    summary.ratingDescription = "Not bad";
-    return summary
-  }
-
-  if (averageTime >= targetTime) {
-    summary.rating = 3;
-    summary.ratingDescription = "Impressive performance";
-    return summary
+    return summary;
+  } catch (err) {
+    return {
+      error: "parameters missing",
+      message: err.message,
+    };
   }
 }
 
-try {
-  const { target, times } = parseValues(process.argv);
-  console.log(calculateExercises(times, target));
-} catch(err) {
-  console.error(err.message);
-}
+export default calculateExercises;
