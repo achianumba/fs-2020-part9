6ac867ff7222bd0c7a53175bc6d22efebb723561
achianumba <chianumbaarinze@gmail.com>
2020-09-07 17:12:32 +0100

    Add '/diagnoses'

--- patientor-backend/api.ts
+++ patientor-backend/api.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { getDiagnoses } from './services/diagnoses';
 
 const api = express.Router();
 
 api.get('/ping', (_req, res) => {
     res.send('pong');
 });
+
+api.get('/diagnoses', (_req, res) => {
+    res.json(getDiagnoses())
+})
 
 export default api;
