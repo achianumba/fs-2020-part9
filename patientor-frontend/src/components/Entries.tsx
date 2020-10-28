60240baf098bae8047c96d50a3db2e2b799f1313
achianumba <chianumbaarinze@gmail.com>
2020-10-28 20:51:23 +0100

    Exercise 9.22

--- patientor-frontend/src/components/Entries.tsx
+++ patientor-frontend/src/components/Entries.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useStateValue } from "../state";
 import { Entry } from "../types";
+import { Card, Icon } from "semantic-ui-react";
+import { allowedNodeEnvironmentFlags } from "process";
 
 const Entries: React.FC<{ entries: Entry[] }> = ({ entries }) => {
   const [{ diagnosis }] = useStateValue();
@@ -11,33 +13,41 @@ const Entries: React.FC<{ entries: Entry[] }> = ({ entries }) => {
 
   return (
     <>
-      {entries.map(({ date, description, diagnosisCodes }, i) => {
+      {entries.map(({ date, description, diagnosisCodes, type }, i) => {
         return (
           <React.Fragment key={i}>
-            {date && (
-              <p>
-                <strong>{date}</strong>
-              </p>
-            )}
-            {description && (
-              <p>
-                <strong>Description: </strong>
-                {description}
-              </p>
-            )}
-            {diagnosisCodes && diagnosisCodes?.length > 0 && (
-              <ul>
-                {diagnosisCodes.map((code, i) => {
-                  return (
-                    code && (
-                      <li key={i}>
-                        {code}: {diagnosis[code]?.name}
-                      </li>
-                    )
-                  );
-                })}
-              </ul>
-            )}
+            <Card fluid style={{ padding: "16px" }}>
+              {date && (
+                <Card.Header>
+                  <strong>{date}</strong>{" "}
+                  {type === "HealthCheck" ? (
+                    <Icon name="doctor" />
+                  ) : type === "OccupationalHealthcare" ? (
+                    <Icon name="stethoscope" />
+                  ) : type === "Hospital" ? (
+                    <Icon name="hospital outline" />
+                  ) : (
+                    ""
+                  )}
+                </Card.Header>
+              )}
+              {description && (
+                <Card.Description>{description}</Card.Description>
+              )}
+              {diagnosisCodes && diagnosisCodes?.length > 0 && (
+                <ul>
+                  {diagnosisCodes.map((code, i) => {
+                    return (
+                      code && (
+                        <li key={i}>
+                          {code}: {diagnosis[code]?.name}
+                        </li>
+                      )
+                    );
+                  })}
+                </ul>
+              )}
+            </Card>
           </React.Fragment>
         );
       })}
